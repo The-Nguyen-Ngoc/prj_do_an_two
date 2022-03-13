6397c774408814ac5513d8681e34279480d9277a
Nguyen Ngoc The <86214462+The-Nguyen-Ngoc@users.noreply.github.com>
2022-03-13 07:22:54 -0700

    Delete PayPalApiTests.java
    delete paypal

--- src/main/java/com/example/prj_do_an_two/entity/PayPalApiTests.java
+++
@@ -1,8 +0,0 @@
-package com.example.prj_do_an_two.entity;
-
-public class PayPalApiTests {
-    private static final String BASE_URL = "https://api.sandbox.paypal.com";
-    private static final String CLIENT_ID = "AS3JVkpgmScMitpIbBfFmslzL-H-Y2OwOrBZL2xphnDisbDf60mLq2lvV10HbqJ3JstWR3ssN-WBMK1V";
-    private static final String GET_ORDER_API = "/v2/checkout/orders/";
-    private static final String CLIENT_SECRET = "EIJd7bpUeA5MZytZiimnCWtsc0BTPNd8F75SdKmf-fxHYscJAdOH5fVcYMqYW2w8uWsh6XEmyna9C2f5";
-}
